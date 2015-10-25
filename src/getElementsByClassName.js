e7e121dd3cdcc0c7c1ee8a91ed9596ce01c47ca0
Joey Holland <joeyholland15@gmail.com>
2015-10-25 15:17:44 -0700

    Added new function to check classlist of current element for class name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,13 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var result = [];
+  function getElements(current) {
+  	if(current.classList !== undefined && current.classList.contains(className)) {
+  		result.push(current);
+  	}
+  }
+  return result; 
 };
+
