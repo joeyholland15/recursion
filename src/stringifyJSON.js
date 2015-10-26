a437d6ae04711e968b241fd229b51a7fb4c76074
Joey Holland <joeyholland15@gmail.com>
2015-10-25 21:29:29 -0700

    Added recursive cases for arrays and objects - tests now pass for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,5 +9,41 @@ var stringifyJSON = function(obj) {
     } else if(typeof obj === "string") {
     	return '"' + obj + '"'
     }
-    
+    if(Array.isArray(obj)) {
+    	if(obj.length === 0) {
+    		obj = '[]';
+    	} else {
+    		var stringified = [];
+    		for(var i = 0; i < obj.length; i++) {
+    			stringified.push(stringifyJSON(obj[i]));
+    		};
+    		var values = stringified.join(",");
+    		return '[' + values + ']';
+    	}
+    } else if (typeof obj === "object") {
+    	var keys = [];
+    	for(var key in obj) {
+    		keys.push(key);
+    	};
+    	if(keys.length === 0) {
+    		return '{}'
+    	} else {
+    		var stringy = '';
+    		for(var i = 0; i < keys.length; i++) {
+    			if(typeof obj[keys[i]] !== "function" && keys[i] !== undefined && obj[keys[i]] !== undefined) {
+    				if(i < keys.length - 1) {
+    					stringy += stringifyJSON(keys[i]) + ":" + stringifyJSON(obj[keys[i]]) + ",";
+    				} else {
+    					stringy += stringifyJSON(keys[i]) + ":" + stringifyJSON(obj[keys[i]]);
+    				}
+    			}
+    		};	
+    		if(stringy === '') {
+    			return '{}';
+    		} else {
+    			return '{' + stringy + '}';	
+    		}
+    	}	
+    }
+    return obj;
 };
